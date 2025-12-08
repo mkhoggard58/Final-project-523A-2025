d0f16138722f5d70eba3ca89b3dfbe1355d39e0b
mkhoggard58 <mkhoggard@icloud.com>
2025-12-08 09:34:38 -0700

    first try

---
+++ setup.R
@@ -0,0 +1,28 @@
+packageLoad <-
+  function(x) {
+    for (i in 1:length(x)) {
+      if (!x[i] %in% installed.packages()) {
+        install.packages(x[i])
+      }
+      library(x[i], character.only = TRUE)
+    }
+  }
+
+
+# vector of packages to load
+packages <- c('tidyverse',
+              'palmerpenguins',
+              'sf',
+              'terra',
+              'tmap',
+              'rmarkdown',
+              'tigris',
+              'elevatr',
+              'rgbif',
+              'soilDB',
+              'plotly',
+              'mapview',
+              "leaflet")
+
+packageLoad(packages)
+
